0399058180cfa0c67a44f98ca54f08afeed713d2
Xica Felix <xicasfelix@gmail.com>
2024-12-10 20:28:07 -0500

    finish and revise logger class

--- logger.py
+++ logger.py
@@ -33,15 +33,15 @@ class Logger(object):
         current_date = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
 
         meta_data = (
-            f'- - - - - - - {virus_name} Simulation - - - - -\n'
-            f'Simulation Date: {current_date}'
+            f'- - - - - - - {virus_name} Simulation - - - - -\n\n'
+            f'Simulation Date: {current_date}\n'
             f'Virus: {virus_name}\n'
             f'Population Size: {pop_size}\n'
             f'Initial Vaccinated Population: {vacc_percentage * 100}%\n'
             f'Initial Infected Population: {initial_infected}\n'
             f'Mortality Rate: {mortality_rate}\n'
             f'Reproductive Rate: {repro_rate}\n'
-            f' - - - - - - - - - - - - - - - - - - - - - - -\n'
+            f' - - - - - - - - - - - - - - - - - - - - - - -\n\n'
         )
 
         with open(self.file_name, 'w') as file:
@@ -52,25 +52,65 @@ class Logger(object):
         # represent all the possible edge cases. Use the values passed along with each person,
         # along with whether they are sick or vaccinated when they interact to determine
         # exactly what happened in the interaction and create a String, and write to your logfile.
-        pass
+        interactions = (
+            f'- - - - - -  INTERACTIONS - STEP NUMBER {step_number} - - - - - - -\n'
+            f'Number Interactions: {number_of_interactions}\n'
+            f'New Infections: {number_of_new_infections}\n\n'
+        )
+
+        with open(self.file_name, 'a') as file:
+            file.write(interactions)
+
 
     def log_infection_survival(self, step_number, population_count, number_of_new_fatalities):
         # TODO: Finish this method. If the person survives, did_die_from_infection
         # should be False.  Otherwise, did_die_from_infection should be True.
         # Append the results of the infection to the logfile
-        pass
-
-    def log_simulation_outcome(self, simulation_time_step, pop_size, total_deaths, simulation_end_reason):
-        summary = (
-            f'- - - - - - - SIMULATION OUTCOME - - - - - - - -'
-            f'The simulation has ended after {simulation_time_step} iterations\n'
-            f'***************************************'
-            f'{simulation_end_reason}\n'
-            f'***************************************'
-            f'Initial Population: {pop_size}'
-            f'Total Deaths: {total_deaths}\n'
-            f'Surviving Population: {pop_size - total_deaths}'
+        survival = (
+            f'- - - - - -  INFECTION SURVIVAL - STEP NUMBER {step_number} - - - -  - \n'
+            f'Total Population: {population_count}\n'
+            f'New Fatalities: {number_of_new_fatalities}\n\n'
         )
 
-        with open(self.file_name, 'w') as file:
+        with open(self.file_name, 'a') as file:
+            file.write(survival)
+
+
+    def log_time_step(self, infected_and_alive, total_deaths, total_interactions, step, pop_size):
+        '''
+            infected_and_alive can never be negative because at the end of the simulation those people with either be vaccinated/immune or dead
+        '''
+        adjusted_infected_and_alive = max(0, infected_and_alive)
+        time_step = (
+            f'- - - - - - - - STEP NUMBER {step} - - - - - - - \n\n'
+            f'Infected: {adjusted_infected_and_alive}\n'
+            f'Vaccinated or Immune: {pop_size - total_deaths - adjusted_infected_and_alive}\n'
+            f'Total Deaths: {total_deaths}\n'
+            f'Total Interactions: {total_interactions}\n\n'
+        )
+
+        with open(self.file_name, 'a') as file:
+            file.write(time_step)
+        
+
+    def log_simulation_outcome(self, simulation_time_step, pop_size, total_deaths):
+        simulation_end_reason = ''
+        if pop_size == total_deaths:
+            simulation_end_reason = 'Entire population is dead'
+        else:
+            simulation_end_reason = 'Entire population is either vaccinated or immune'
+
+        summary = (
+            f'- - - - - - - SIMULATION OUTCOME - - - - - - - -\n\n'
+            f'The simulation has ended after {simulation_time_step} iterations\n'
+            f'***************************************\n'
+            f'{simulation_end_reason}\n'
+            f'***************************************\n'
+            f'Initial Population: {pop_size}\n'
+            f'Total Deaths: {total_deaths}\n'
+            f'Surviving Population: {pop_size - total_deaths}\n'
+            f'Calculated Mortality Rate: {total_deaths / pop_size}'
+        )
+
+        with open(self.file_name, 'a') as file:
             file.write(summary)
